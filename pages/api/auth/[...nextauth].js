880244228249e3397221bc30ed95406e6d656f84
KawaseFuyuki <kawasefuyuki@gmail.com>
2026-04-30 07:47:04 +0530

    Add guilds scope and callbacks

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,12 +1,28 @@
 import NextAuth from "next-auth"
 import DiscordProvider from "next-auth/providers/discord"
 
-export default NextAuth({
+export const authOptions = {
   providers: [
     DiscordProvider({
       clientId: process.env.DISCORD_CLIENT_ID,
-      clientSecret: process.env.DISCORD_CLIENT_SECRET
+      clientSecret: process.env.DISCORD_CLIENT_SECRET,
+      authorization: { params: { scope: 'identify email guilds' } }
     })
   ],
   secret: process.env.NEXTAUTH_SECRET,
-})
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token
+      }
+      return token
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken
+      session.user.id = token.sub
+      return session
+    }
+  }
+}
+
+export default NextAuth(authOptions)
