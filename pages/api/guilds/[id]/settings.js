d2fb3c175da860de306f86fb5a9529902e2f8b57
KawaseFuyuki <kawasefuyuki@gmail.com>
2026-04-30 09:19:02 +0530

    Refactor welcome and ticket settings structure

--- pages/api/guilds/[id]/settings.js
+++ pages/api/guilds/[id]/settings.js
@@ -5,7 +5,6 @@ export default async function handler(req, res) {
 
   if (req.method === 'GET') {
     const settings = serverSettings[id] || {
-      // 1. WELCOME SETTINGS
       welcomeEnabled: true,
       welcomeMessage: 'Welcome {user} to {server}! 🐱',
       welcomeChannel: '',
@@ -13,57 +12,29 @@ export default async function handler(req, res) {
       welcomeEmbedColor: '#ff69b4',
       welcomeImageUrl: '',
       welcomeEmoji: '👋',
-      welcomeEmojiCustom: '', // NEW: <:name:id> ya <a:name:id>
-      welcomeEmojiType: 'unicode', // NEW: unicode ya custom
+      welcomeEmojiCustom: '',
+      welcomeEmojiType: 'unicode',
       autoRole: '',
       prefix: '+',
-      
-      // 2. TICKET SYSTEM - MULTIPLE BUTTONS WITH CUSTOM EMOJI
       ticketEnabled: false,
       ticketPanelTitle: 'Support Center 🎫',
       ticketPanelDesc: 'Select a category below to create a ticket',
       ticketPanelColor: '#5865F2',
       ticketPanelChannelId: '',
       ticketTypes: [
-        {
-          id: 'help',
-          name: 'General Help',
-          emoji: '🎫',
-          emojiCustom: '', // NEW
-          emojiType: 'unicode', // NEW
-          buttonColor: 'PRIMARY',
-          categoryId: '',
-          supportRoleId: '',
-          description: 'Get help with general questions'
-        },
-        {
-          id: 'prize',
-          name: 'Prize Claim',
-          emoji: '🎁',
-          emojiCustom: '',
-          emojiType: 'unicode',
-          buttonColor: 'SUCCESS',
-          categoryId: '',
-          supportRoleId: '',
-          description: 'Claim your giveaway prizes'
-        }
+        { id: 'help', name: 'General Help', emoji: '🎫', emojiCustom: '', emojiType: 'unicode', buttonColor: 'PRIMARY', categoryId: '', supportRoleId: '', description: 'Get help with general questions' },
+        { id: 'prize', name: 'Prize Claim', emoji: '🎁', emojiCustom: '', emojiType: 'unicode', buttonColor: 'SUCCESS', categoryId: '', supportRoleId: '', description: 'Claim your giveaway prizes' }
       ],
-      
-      // 3. REACTION ROLES WITH CUSTOM EMOJI
       reactionRolesEnabled: false,
       reactionRolesChannelId: '',
       reactionRolesMessageId: '',
-      reactionRolesData: [], // [{emoji: '😎', emojiCustom: '', emojiType: 'unicode', roleId: '123'}]
-      
-      // 4. AUTO MOD
+      reactionRolesData: [],
       autoModEnabled: false,
       autoModBadWords: '',
       autoModSpamLimit: 5,
       autoModCapsPercent: 70,
       autoModLinkBlock: true,
       autoModPunishment: 'timeout',
-      
-      // 5. LOGGING
       logsEnabled: false,
       logsChannelId: '',
       logJoins: true,
